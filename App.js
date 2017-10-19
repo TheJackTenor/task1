831af35f6ebd5caa057442d6e82f4fd34ed7db99
TheJackTenor <dhihanlaksana@live.com>
2017-10-19 21:59:04 +0700

    task1

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+
+export default class App extends React.Component {
+  render() {
+    let dataDiri = {
+      nama: 'Dhihan Laksana Aji Anggana',
+      batch : '#1'
+    };
+    let foto = {
+      uri: 'http://lh3.googleusercontent.com/-6f6paMaVF64/AAAAAAAAAAI/AAAAAAAAAiA/czvGPXSoju4/s128-c0x00000000-cc-rp-mo/photo.jpg'
+    };
+    return (
+      <View style={styles.container}>
+        <Text>Nama: </Text>
+        <Text style={styles.content}>{dataDiri.nama}</Text>
+        <Image source={foto} style={{height:'30%',width:'55%'}}/>
+        <Text>Batch:</Text>
+        <Text style={styles.content}>{dataDiri.batch}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    fontSize:20,
+    fontWeight:'bold'
+  }
+});
